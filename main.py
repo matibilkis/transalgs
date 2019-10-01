4a442e26a508ef0b1da193d0127a33e7bdb5f24a
cooper-cooper <matiasbilkis@gmail.com>
2019-10-01 19:34:31 +0200

    with averagedistance

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class CityGraph():
-    def __init__(self, ratio=3,length=10, width=10):
+    def __init__(self, ratio=3,length=100, width=100):
         self.city_graph = self.create_lattice(ratio, N=length, M=width)
-        self.mother = self.give_mother
+        self.mother = self.give_mother()
 
     def add_path_bi(self,g,a,b,subnet="father"):
         """ Take a graph g and creates a bidirectional edge from node a to node b """
@@ -26,9 +27,9 @@ class CityGraph():
                 city_graph.add_nodes_from([(i1,i2)])
                 if (i1%ratio ==0)& (i2%ratio==0):
                     if i2 !=M:
-                        self.add_path_bi(city_graph,(i1,i2),(i1,i2+ratio),subnet="mother")
+                        self.add_path_bi(city_graph,(i1,i2),(i1,min(M,i2+ratio)),subnet="mother")
                     if i1 !=0:
-                        self.add_path_bi(city_graph,(i1,i2),(i1-ratio,i2), subnet="mother")
+                        self.add_path_bi(city_graph,(i1,i2),(max(i1-ratio,0),i2), subnet="mother")
                 if i2!=M:
                     self.add_path_bi(city_graph, (i1,i2),(i1,i2+1),subnet="father")
                 if i1!=0:
@@ -39,6 +40,7 @@ class CityGraph():
         """ retrieves a DiGraph with all possible paths in the transport system """
         madre = nx.DiGraph(((source, target, attr) for source, target, attr
                             in self.city_graph.edges(data=True) if attr['net'] == "mother"))
+        return madre
 
     def random_closed_path(self,terminal_node):
         """ retrieves a random closed path from node to itself"""
@@ -59,6 +61,22 @@ class CityGraph():
                path_complete = True
         return path
 
+    def compute_fitness(self, path=None):
+        """ Example: take path = self.random_closed_path((0,0))"""
+        average_path=0
+        c=0
+        f = self.city_graph.copy()
+        if path == None:
+            path = self.random_closed_path((0,0))
+        f.add_edges_from(path)
+        for node1 in list(f.nodes):
+            nodes2=list(f.nodes)
+            nodes2.remove(node1)
+            for node2 in nodes2:
+                average_path += nx.shortest_path_length(f,node1,node2)
+                #average_path += nx.shortest_path_length(f,node2,node1) #Notice this is useless if ida y vuelta
 
-    def compute_fitness(self, path):
-        return
+                c+=1
+        return average_path/c
+
+    
