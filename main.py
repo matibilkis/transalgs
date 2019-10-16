8e63f5f93a7e14b8e3834bf50aefb34ea1597c9d
Matias <matias@GIQ-GOLEM.local>
2019-10-16 14:59:47 +0200

    I can merge two given paths at the lth position if they satisfy the merging conditions -also called sex conditions -

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class CityGraph():
     def __init__(self, ratio=3,length=100, width=100):
         self.city_graph = self.create_lattice(ratio, N=length, M=width)
         self.mother = self.give_mother()
+        self.father = self.give_father()
 
     def add_path_bi(self,g,a,b,subnet="father"):
         """ Take a graph g and creates a bidirectional edge from node a to node b """
@@ -15,7 +16,7 @@ class CityGraph():
         return
 
 
-    def create_lattice(self,ratio,N=4,M=4):
+    def create_lattice(self,ratio,N,M):
         """ This function returns a city graph
         that has two edge classes: father or mother. Father
         refers to walking points, where mother to points connected with
@@ -26,11 +27,11 @@ class CityGraph():
             for i2 in range(N+1):
                 city_graph.add_nodes_from([(i1,i2)])
                 if (i1%ratio ==0)& (i2%ratio==0):
-                    if i2 !=M:
-                        self.add_path_bi(city_graph,(i1,i2),(i1,min(M,i2+ratio)),subnet="mother")
+                    if i2 !=N:
+                        self.add_path_bi(city_graph,(i1,i2),(i1,min(N,i2+ratio)),subnet="mother")
                     if i1 !=0:
                         self.add_path_bi(city_graph,(i1,i2),(max(i1-ratio,0),i2), subnet="mother")
-                if i2!=M:
+                if i2!=N:
                     self.add_path_bi(city_graph, (i1,i2),(i1,i2+1),subnet="father")
                 if i1!=0:
                     self.add_path_bi(city_graph,(i1,i2),(i1-1,i2), subnet="father")
@@ -42,6 +43,12 @@ class CityGraph():
                             in self.city_graph.edges(data=True) if attr['net'] == "mother"))
         return madre
 
+    def give_father(self):
+        """ retrieves a DiGraph with all possible paths in the transport system """
+        padre = nx.DiGraph(((source, target, attr) for source, target, attr
+                            in self.city_graph.edges(data=True) if attr['net'] == "father"))
+        return padre
+
     def random_closed_path(self,terminal_node):
         """ retrieves a random closed path from node to itself"""
         path=[]
@@ -51,9 +58,11 @@ class CityGraph():
         c=0
         while not path_complete:
             next_node = random.choice(list(self.mother.neighbors(current_node)))
-            if c==1:
-                while next_node == terminal_node: #avoid one-length path
-                    next_node = random.choice(list(self.mother.neighbors(current_node)))
+            #i comment this as the program shoul realize a bad path is a way and return and all those things...
+            #If later i've problems with convergence, i'll take a look
+#            if c==1:
+ #               while next_node == terminal_node: #avoid one-length path
+  #                  next_node = random.choice(list(self.mother.neighbors(current_node)))
             path.append((current_node,next_node))
             current_node = next_node
             c+=1
@@ -79,4 +88,44 @@ class CityGraph():
                 c+=1
         return average_path/c
 
-        
+    def check_paths(self,paths, changePoints):
+        if isinstance(changePoints,list) != True:
+            changePoints = [changePoints]
+        cop = paths.copy()
+        for ind,i in enumerate(paths):
+            if len(i)<(max(changePoints)+1):
+                print("Path {} discarded".format(str(ind)))
+                cop.remove(i)
+        if len(cop)<(len(changePoints)+1): #notice it's +1 as the path may be length changePoints
+            #to be serious we should put changePoint strictly higher than shortest path distance... but from which to which ?
+            return False, []
+        else:
+            return True, cop
+
+    def crop_and_paste(self, paths, changePoints=5):
+        if self.check_paths(paths,changePoints)[0]==True:
+            motherAndPaths = self.mother.copy()
+            for p in paths:
+                motherAndPaths.add_edges_from(p)
+            if isinstance(changePoints,list) != True:
+                changePoints = [changePoints]
+            path = tuple(paths[0][:changePoints[0]])
+
+            print("first_part, path1 : ",path)
+            extremePath1= paths[0][changePoints[0]][1]
+            extremePath2 = paths[1][changePoints[0]+1][0]
+
+            shortest_connection = tuple(nx.shortest_path(motherAndPaths, extremePath1, extremePath2))
+            path_edg = (tuple([path[-1][-1], shortest_connection[0]]),)
+            for i in range(len(shortest_connection)-1):
+                path_edg += tuple([(tuple([(shortest_connection[i]),(shortest_connection[i+1])]))])
+
+#             print("shortest-connection, ", shortest_connection)
+#             print("connection: ", path_edg)
+            path += path_edg
+            path += tuple(paths[1][(changePoints[0]+1):])
+            return True, path
+        else:
+            print("Paths do not satisfy sex onditions (maybe they are not hot enough)")
+            return False, ()
+        #I'll make it for only one changePoint, but it should not be difficult to extend this
